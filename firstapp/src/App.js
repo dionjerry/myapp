551a9b01f0f5556088f3335c0b6c971c9259e3d9
dionjerry <dionjerry442@gmail.com>
2022-10-25 15:12:24 +0000

    new file:   firstapp/.gitignore 	new file:   firstapp/README.md 	new file:   firstapp/package-lock.json 	new file:   firstapp/package.json new file:   firstapp/public/favicon.ico 	new file:   firstapp/public/index.html 	new file:   firstapp/public/logo192.png 	new file:   firstapp/public/logo512.png 	new file:   firstapp/public/manifest.json 	new file:   firstapp/public/robots.txt 	new file:   firstapp/src/App.css 	new file:   firstapp/src/App.js 	new file:   firstapp/src/App.test.js 	new file:   firstapp/src/core/Board.js 	new file:   firstapp/src/core/Game.js 	new file:   firstapp/src/core/Square.js 	new file:   firstapp/src/index.css 	new file:   firstapp/src/index.js 	new file:   firstapp/src/logo.svg 	new file:   package-lock.json

---
+++ firstapp/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Board from './core/Board';
+import Game from './core/Game';
+
+function App() {
+  return (
+    <div>
+     <Game />
+
+      
+    </div>
+    
+  );
+}
+
+export default App;
